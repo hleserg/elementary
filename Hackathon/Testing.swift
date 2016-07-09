6e6b017f2935cc26be6aa0df15b5581fc75208fc
Sergey Khlebnikov <hleserg@gmail.com>
2016-07-09 18:11:34 +0300

    1

---
+++ Hackathon/Testing.swift
@@ -0,0 +1,14 @@
+//
+//  Testing.swift
+//  Hackathon
+//
+//  Created by Sergey Khlebnikov on 09.07.16.
+//  Copyright © 2016 Sergey Khlebnikov. All rights reserved.
+//
+
+import Foundation
+
+class Testing {
+    let theme: String
+    let 
+}
